8c7725475dd2202becd8ccd431803269ecaa9cb3
Justin Hui <jhui@lcs.on.ca>
2023-11-02 14:08:54 -0400

    Initial Commit

---
+++ FavouriteAlbums/FavouriteAlbumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteAlbumsApp.swift
+//  FavouriteAlbums
+//
+//  Created by Justin Hui on 2/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteAlbumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
