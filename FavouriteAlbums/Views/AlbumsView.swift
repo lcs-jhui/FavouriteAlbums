279db2ad6805ab81bdde15f6ea0ab8c184d10376
Justin Hui <jhui@lcs.on.ca>
2023-11-02 14:21:05 -0400

    Deleted content view and created a new group called views and created a new swiftUI file called albumsview

---
+++ FavouriteAlbums/Views/AlbumsView.swift
@@ -0,0 +1,27 @@
+//
+//  AlbumsView.swift
+//  FavouriteAlbums
+//
+//  Created by Justin Hui on 2/11/2023.
+//
+
+import SwiftUI
+
+struct AlbumsView: View {
+    
+    //MARK: Stored Properties
+    
+    //MARK: Computed Properties
+    var body: some View {
+        
+            HStack{
+                
+                Text("Hello, World!")
+                
+            }
+    }
+}
+
+#Preview {
+    AlbumsView()
+}
