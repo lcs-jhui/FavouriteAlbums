c4db1974be5667dfd2836a6f95e4f17641cc6287
Justin Hui <jhui@lcs.on.ca>
2023-11-02 14:32:01 -0400

    Added three textfields where the user can input strings, and added a picker where the user can choose a rating

--- FavouriteAlbums/Views/AlbumsView.swift
+++ FavouriteAlbums/Views/AlbumsView.swift
@@ -10,13 +10,44 @@ import SwiftUI
 struct AlbumsView: View {
     
     //MARK: Stored Properties
+    @State var albumName = ""
+    @State var artistName = ""
+    @State var albumImageLink = ""
+    @State var rating = 3
     
     //MARK: Computed Properties
     var body: some View {
         
             HStack{
                 
-                Text("Hello, World!")
+                VStack{
+                    
+                    TextField("Enter Album Name", text: $albumName)
+                        .textFieldStyle(.roundedBorder)
+
+                    TextField("Enter Artist Name", text: $artistName)
+                        .textFieldStyle(.roundedBorder)
+
+                    TextField("Enter Album Image Link", text: $albumImageLink)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Picker("", selection: $rating){
+                        Text("★").tag(1)
+                        Text("★★").tag(2)
+                        Text("★★★").tag(3)
+                        Text("★★★★").tag(4)
+                        Text("★★★★★").tag(5)
+                    }
+                    .pickerStyle(.segmented)
+                    
+                }
+                .padding()
+                
+                List {
+                    Text("Item 1")
+                    Text("Item 2")
+                    Text("Item 3")
+                }
                 
             }
     }
